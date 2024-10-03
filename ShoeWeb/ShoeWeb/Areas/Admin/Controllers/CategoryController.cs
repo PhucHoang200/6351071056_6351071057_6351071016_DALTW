ba63f7eb7b50da39b0cb22669bd4d0eeebde6d2f
dat015 <datnguyen151004@gmail.com>
2024-10-03 22:25:57 +0700

    update admin

---
+++ ShoeWeb/ShoeWeb/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ShoeWeb.Data;
+using ShoeWeb.Areas.Admin.Admin_ViewModel;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace ShoeWeb.Areas.Admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public CategoryController   () : this(new ApplicationDbContext())
+        {
+
+        }
+        public async Task<CategoryVM> GetCategoriese()
+        {
+            return new CategoryVM()
+            {
+                categories = await _db.categories.ToListAsync()
+
+            };
+
+        }
+        public async Task<ActionResult> Index()
+        {
+
+            var cate = await GetCategoriese();
+            return View(cate);
+        }
+    }
+}
---
+++ ShoeWeb/ShoeWeb/Migrations/202409230651099_addProductToDb1.cs
@@ -0,0 +1,16 @@
+﻿namespace ShoeWeb.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addProductToDb1 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
