18cf823be636f6b04cd3fa7bf46c8444c9e98af0
dat015 <datnguyen151004@gmail.com>
2024-10-17 21:59:20 +0700

    update

---
+++ ShoeWeb/ShoeWeb/Areas/Admin/Admin_ViewModel/ProductVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ShoeWeb.Models;
+
+namespace ShoeWeb.Areas.Admin.Admin_ViewModel
+{
+    public class ProductVM
+    {
+        public IEnumerable<Product> Products { get; set; }
+
+    }
+}
--- ShoeWeb/ShoeWeb/Areas/Admin/Controllers/ProductController.cs
+++ ShoeWeb/ShoeWeb/Areas/Admin/Controllers/ProductController.cs
@@ -1,18 +1,38 @@
-﻿using System;
+﻿using ShoeWeb.Data;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using System.Threading.Tasks;
+using ShoeWeb.Areas.Admin.Admin_ViewModel;
 namespace ShoeWeb.Areas.Admin.Controllers
 {
    
     public class ProductController : Controller
     {
-        
-        public ActionResult Index()
+        private readonly ApplicationDbContext _db;
+        public ProductController(ApplicationDbContext db)
         {
-            return View();
+            _db = db;
+        }
+        public ProductController() : this(new ApplicationDbContext())
+        {
+
+        }
+
+        public async Task<ProductVM> GetProductVM()
+        {
+            return new ProductVM()
+            {
+                Products = await _db.products.ToListAsync(),
+            };
+        }
+        public async Task<ActionResult> Index()
+        {
+            var productVM = await GetProductVM();
+            return View(productVM);
         }
     }
 }
